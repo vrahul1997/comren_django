921cca0f29353c14cbbedc52c91b82ec9102d8de
vrahul1997 <vijayvenu1997@gmail.com>
2022-03-25 18:12:37 -0300

    static added

--- comren/settings.py
+++ comren/settings.py
@@ -146,7 +146,7 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
